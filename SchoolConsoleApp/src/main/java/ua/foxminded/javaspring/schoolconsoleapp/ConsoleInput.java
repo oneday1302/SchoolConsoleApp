45dbf282540fe4badfd6a107916a587c620782b4
oneday1302 <kovalchukvladyslav2gmail.com>
2022-11-21 00:13:12 +0100

    added class ConsoleInput

---
+++ SchoolConsoleApp/src/main/java/ua/foxminded/javaspring/schoolconsoleapp/ConsoleInput.java
@@ -0,0 +1,16 @@
+package ua.foxminded.javaspring.schoolconsoleapp;
+
+import java.util.Scanner;
+
+public class ConsoleInput {
+    
+    public String getLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
