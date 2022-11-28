6d26ca7d3ec44a63900a99a8fa736eec24ae310a
oneday1302 <kovalchukvladyslav2gmail.com>
2022-11-28 12:38:23 +0100

    added class DataBaseUtility

---
+++ SchoolConsoleApp/src/main/java/ua/foxminded/javaspring/schoolconsoleapp/DataBaseUtility.java
@@ -0,0 +1,27 @@
+package ua.foxminded.javaspring.schoolconsoleapp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class DataBaseUtility extends BasicDataSource {
+    private static final Properties prop = new Properties();
+
+    public DataBaseUtility(String propertyFileName) {
+        if (propertyFileName == null) {
+            throw new IllegalArgumentException("Param cannot be null.");
+        }
+        try (InputStream input = this.getClass().getResourceAsStream(propertyFileName)) {
+            prop.load(input);
+            
+            this.setDriverClassName(prop.getProperty("driverName"));
+            this.setUrl(prop.getProperty("url"));
+            this.setUsername(prop.getProperty("user"));
+            this.setPassword(prop.getProperty("password"));
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
